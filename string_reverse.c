62dcd9a4a83f4f3a6742fee47ba12a175d6d821a
Santosh-Nijaguli <140332169+Santosh-Nijaguli@users.noreply.github.com>
2023-08-06 13:44:41 +0530

    Update string_reverse.c

--- string_reverse.c
+++ string_reverse.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
+int string_reverse()
+{
+   char s[1000], r[1000];
+   int begin, end, count = 0;
+   printf("Input a string:");
+   gets(s);
+   while (s[count] != '\0')
+      count++;    // Calculating string length
 
-int string_reverse() {
-  
-char string1[100]="tutorial", reversed_string[100];
-int i, j, count = 0;
 
-//original string
+   end = count - 1;
+   for (begin = 0; begin < count; begin++) {
+      r[begin] = s[end];
+      end--;
+   }
 
-printf("\n Given String = %s", string1);
 
-// loop to calculate the length of the string
-while (string1[count] != '\0') {
-  count++;
+   r[begin] = '\0';
+
+
+   printf("%s\n", r);
+
+
+   return 0;
 }
-
-j = count - 1;
-
-//assigning the value to characters of a new string
-for (i = 0; i < count; i++) {
-  reversed_string[i] = string1[j];
-  j--;
-}
-
-printf("\n Reversed String = %s", reversed_string);
-}
-
